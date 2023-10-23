d917dd58d257c0cd56156eeffb34a221d29dc381
onjoseph <on.josephinfo@gmail.com>
2023-10-23 14:35:04 +0100

    Added get routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Bun Dev, I am going to build RESTful APIs")
+  .get('/post/:id', ({ params: { id } }) => {
+    return { id, title: 'Learn Bun!' };
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
